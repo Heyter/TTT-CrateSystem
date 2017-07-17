8b35fdf5fcd7a325297de772420a06fb2e6046eb
Heyter <ll2.lol@yandex.ru>
2017-07-17 06:07:50 +0300

    Delete MaterialSetter.lua

--- lua/crates/MaterialSetter.lua
+++
@@ -1,8 +0,0 @@
---[[-------------------------------------------------------------------------
-This just needed to be shared and I didn't want to put it in shared.lua
----------------------------------------------------------------------------]]
-
--- Noice and simple :)... It sets the entitys material if needed.
-function CrateWeaponMaterial( ent, MaterialStr )
-   ent:SetMaterial( MaterialStr, true )
-end 
